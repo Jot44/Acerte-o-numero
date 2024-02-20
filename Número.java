253c5d30d6e2fc3d227bcc3e3b4ec6c8accfabe1
João Victor Franco <109479314+Jot44@users.noreply.github.com>
2024-02-20 19:02:42 -0300

    Add files via upload

---
+++ Número.java
@@ -0,0 +1,27 @@
+package empresa;
+
+import java.util.Scanner;
+
+public class Número {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Digite seu palpite:");
+		int palpite = teclado.nextInt();
+		
+		while(palpite!=10000) {
+			if(palpite<10000) {
+				System.out.println("Um pouco mais...");
+			}
+			else {
+				System.out.println("Um pouco menos...");
+			}
+			System.out.println("Digite seu pr�ximo palpite");
+			palpite = teclado.nextInt();
+		}
+		System.out.println("Seu palpite est� correto");
+
+	}
+	
+
+}
